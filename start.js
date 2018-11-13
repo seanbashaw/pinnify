d974ed6507bbd655193a53d82063fbf8626ba7b5
Sean Bashaw <seanbshw@gmail.com>
2018-11-12 23:18:32 -0500

    Updated start.js
    Added abstraction for authentication for heroku app

--- start.js
+++ start.js
@@ -1,24 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 client.once('ready', () => {
     console.log('Ready!');
 });
-client.login(/*Insert Authentication Token Here*/);
+client.login(process.env.auth);
 client.on('message', message => {
     if (message.type === "PINS_ADD" && message.author.bot === true) {
         message.delete();
     }
 });
+
 client.on('messageReactionAdd', (reaction, user) => {
     if (!reaction.message.pinned && reaction.count >= 4 && reaction.emoji.name === '📌') {
-        console.log("SUCCESS!");
-        reaction.message.react('✔️')
+        reaction.message.react('✅')
         reaction.message.pin();
     }
 });
 client.on('messageReactionRemove', (reaction, user) => {
     if (reaction.count < 4 && reaction.emoji.name === '📌') {
-        var check = reaction.message.reactions.find(em => em.emoji.name === '✔️');
+        var check = reaction.message.reactions.find(em => em.emoji.name === '✅');
         if (check.me) {
             reaction.message.unpin();
             check.remove();
