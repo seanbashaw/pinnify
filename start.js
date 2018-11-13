dabf8a772b5ecb4e18bd200599de32f05c7f0356
Sean Bashaw <seanbshw@gmail.com>
2018-11-13 01:12:21 -0500

    Update start.js
    Replaced undefined with null

--- start.js
+++ start.js
@@ -20,7 +20,7 @@ client.on('messageReactionAdd', (reaction, user) => {
 client.on('messageReactionRemove', (reaction, user) => {
     if (reaction.count < process.env.pin_trigger && reaction.emoji.name === '📌') {
         var check = reaction.message.reactions.find(em => em.emoji.name === '✅');
-        if (typeof check !== undefined)
+        if (check !== null)
         	if (check.me) {
             	reaction.message.unpin();
             	check.remove();
